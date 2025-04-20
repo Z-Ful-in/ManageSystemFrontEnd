296739d6821e67a8e916c113028de8da6c08dd3d
ZIHIKA <1918938521@qq.com>
2025-04-20 13:00:36 +0800

    concatenate relative path to server url

--- app/src/main/java/com/example/trafficviolationpicturemanagementsystem/model/UserImage.kt
+++ app/src/main/java/com/example/trafficviolationpicturemanagementsystem/model/UserImage.kt
@@ -1,10 +1,14 @@
 package com.example.trafficviolationpicturemanagementsystem.model
 
+val BASE_URL = "http://10.0.2.2:8000"
+
 data class UserImage(
     val id: Int,
     val url: String,
     val description: String,
-)
+){
+    fun fullUrl() = BASE_URL + url
+}
 
 data class UserImageResponse(
     val success: Boolean,
--- app/src/main/java/com/example/trafficviolationpicturemanagementsystem/ui/home/ManagementAppPortrait.kt
+++ app/src/main/java/com/example/trafficviolationpicturemanagementsystem/ui/home/ManagementAppPortrait.kt
@@ -50,7 +50,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -58,7 +57,6 @@ import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
-import com.example.trafficviolationpicturemanagementsystem.R
 import com.example.trafficviolationpicturemanagementsystem.model.UserImage
 import com.example.trafficviolationpicturemanagementsystem.ui.state.UploadDialogState
 import com.example.trafficviolationpicturemanagementsystem.viewmodel.HomeViewModel
@@ -263,7 +261,7 @@ fun ImageWithText(
         ) {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
-                    .data(imageItem.url)
+                    .data(imageItem.fullUrl())
                     .crossfade(true)
                     .build(),
                 contentDescription = imageItem.description,
